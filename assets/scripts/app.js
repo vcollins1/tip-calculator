491d30e516f64fdd7bffe55df535557ad9937bbb
Trelle823 <vontrellecollins@gmail.com>
2023-12-03 01:34:38 -0500

    implement tip calculator with javascript

---
+++ assets/scripts/app.js
@@ -0,0 +1,36 @@
+
+const form = document.querySelector(".calculator__inputs");
+const tips = document.querySelector("#tips");
+const tipAmount = document.querySelector(".money--tip");
+const total = document.querySelector(".money--total");
+
+tips.addEventListener("click", e => {
+    if (e.target.classList.contains("tip")) {
+        document.querySelector(".tip--active").classList.remove("tip--active");
+        e.target.classList.add("tip--active");
+    }
+});
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    const bill = document.querySelector("#bill").value == '' ? 0 : parseFloat(document.querySelector("#bill").value);
+
+        let percentage;
+    if (document.querySelector(".tip--active").classList.contains("tip--input")) {
+        percentage = parseFloat(document.querySelector(".tip--active").value);
+        console.log("perPerson")
+    } else {
+        percentage = parseFloat(document.querySelector(".tip--active").innerText.replace('%', ''));
+    }
+
+
+
+    const people = document.querySelector("#people").value == '' ? 0 : parseFloat(document.querySelector("#people").value);
+
+    const eachTip = (bill * (percentage / 100) / people).toFixed(2);
+    const billSplit = parseFloat(bill / people).toFixed(2);
+    const perPerson = (parseFloat(billSplit) + parseFloat(eachTip)).toFixed(2);
+
+    tipAmount.innerHTML = eachTip;
+    total.innerHTML = perPerson;
+});
